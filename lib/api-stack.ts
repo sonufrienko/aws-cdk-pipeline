78cf10636cfdc044f68d9a6524247ad63039edaf
Sergey Onufrienko <sergey@onufrienko.com>
2021-09-06 00:11:12 +0300

    Added api stack

---
+++ lib/api-stack.ts
@@ -0,0 +1,15 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Function, InlineCode, Runtime } from 'aws-cdk-lib/lib/aws-lambda';
+import { Construct } from 'constructs';
+
+export class ApiStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    new Function(this, 'LambdaFunction', {
+      runtime: Runtime.NODEJS_14_X,
+      handler: 'index.handler',
+      code: new InlineCode('exports.handler = _ => "Hello, CDK";'),
+    });
+  }
+}
